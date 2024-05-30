2cc7998a9d718a13cfbf2d647aab8caae3a9f122
David <dguillory62888@gmail.com>
2024-05-30 10:16:17 -0500

    meta update

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Welcome To The Five0Forge Gallery!" />
+  <HelloWorld msg="Welcome To The Five0Forge Gallery" />
 </template>
 
 <style scoped>
